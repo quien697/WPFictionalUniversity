c27e9c9e9e4af123bf269ffd91d432d5cc0d02ab
Quien <quien697@gmail.com>
2024-07-26 19:29:54 -0700

    feat: Live Search & WordPress REST API
    1. add a javascript file of search with jquery

--- wp-content/themes/wp-fictional-university/functions.php
+++ wp-content/themes/wp-fictional-university/functions.php
@@ -9,14 +9,18 @@
  */
 function wp_fictional_university_files() {
     // Style
-    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('wp_fictional_university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('wp_fictional_university_extra_styles', get_theme_file_uri('/build/index.css'));
 
     // Script
-    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=' . $_ENV['GOOGLE_MAP_KEY'], NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('google_map', '//maps.googleapis.com/maps/api/js?key=' . $_ENV['GOOGLE_MAP_KEY'], NULL, '1.0', true);
+    wp_enqueue_script('wp_fictional_university_main_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+
+    wp_localize_script('wp_fictional_university_main_js', 'wpFictionalUniversityData', array(
+        'root_url' => get_site_url(),
+    ));
 }
 add_action('wp_enqueue_scripts', 'wp_fictional_university_files');
 
